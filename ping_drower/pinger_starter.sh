09c4c197cff28af8df4de36f703ebd30df53555a
Grigory Kuzovnikov <kuzovnikov@immo.ru>
2011-09-09 09:28:49 +0400

    garbage

--- ping_drower/pinger_starter.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ -f /var/lock/pinger ]; 
-then    echo 'крон уже запущен';
-	echo `date` 'крон уже запущен' > /var/log/pinger.log
-else    touch /var/lock/pinger;
-	/root/cron/pinger/ping_cron_multithread_v3.py	
-	rm /var/lock/pinger; 
-fi;
--- renew_mac.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-/usr/sbin/arp -n | grep "$1 " | /usr/bin/tr -s " " | /usr/bin/cut -d " " -f 3 | /usr/bin/tr -d "\n"
